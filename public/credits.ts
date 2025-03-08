5a8ccd1ff53412a57d8b77b0c8e07be8f1c968ea
BeastMasterGrinder <m.farjad2004@gmail.com>
2025-03-08 20:00:15 +0500

    feat: Add credits for the 3d object of loading

---
+++ public/credits.ts
@@ -0,0 +1,6 @@
+const credits = [
+  {
+    name: "3D art",
+    description: "'Allah' (https://skfb.ly/6uIwB) by lam_m_zack is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).",
+  },
+];

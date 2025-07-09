f3c327af6305844dff5046a0def66fb981b43573
BeastMasterGrinder <m.farjad2004@gmail.com>
2025-07-09 05:59:04 +0500

    refactor[metadata][blog][slug]: Remove Duplicate Function from page.tsx and place it in layout.tsx. Why vibe it?

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -1,41 +1,64 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 import getBlogData from "@/lib/blog/getBlogData";
 
 const defaultUrl = process.env.NEXT_PUBLIC_APP_URL
-  ? `https://www.${process.env.NEXT_PUBLIC_APP_URL}`
-  : "http://localhost:3000";
+  ? `https://${process.env.NEXT_PUBLIC_APP_URL}`
+  : "https://qalbjournal.com";
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-  const blog = await getBlogData(params.slug);
+type Params = Promise<{ slug: string }>
+
+export async function generateMetadata(
+  props: {
+    params: Params
+  },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const blog = await getBlogData((await props.params).slug);
   
   if (!blog) {
     return {
-      title: "Blog Post Not Found | Qalb",
-      description: "The requested blog post could not be found.",
-    };
+      title: 'Blog Post Not Found',
+      description: 'The requested blog post could not be found.'
+    }
   }
 
+  // Get the parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
   return {
-    title: `${blog.title} | Qalb`,
+    title: blog.title,
     description: blog.excerpt || blog.content.substring(0, 160),
+    authors: [{ name: blog.author || 'Qalb Team' }],
     openGraph: {
       title: blog.title,
       description: blog.excerpt || blog.content.substring(0, 160),
-      url: `${defaultUrl}/blog/${params.slug}`,
-      siteName: "Qalb",
-      locale: "en_US",
-      type: "article",
-      authors: blog.author ? [blog.author] : ["Qalb Team"],
+      type: 'article',
       publishedTime: blog.created_at,
-      modifiedTime: blog.created_at,
+      authors: [blog.author || 'Qalb Team'],
+      url: `${defaultUrl}/blog/${(await props.params).slug}`,
+      siteName: 'Qalb',
+      locale: 'en_US',
+      images: blog.coverImage ? [
+        {
+          url: blog.coverImage,
+          width: 1200,
+          height: 630,
+          alt: blog.title,
+        },
+        ...previousImages
+      ] : previousImages,
     },
     twitter: {
-      card: "summary_large_image",
+      card: 'summary_large_image',
       title: blog.title,
       description: blog.excerpt || blog.content.substring(0, 160),
-      creator: "@QalbApp",
+      creator: '@QalbApp',
+      images: blog.coverImage ? [blog.coverImage] : undefined,
     },
-  };
+    alternates: {
+      canonical: `/blog/${(await props.params).slug}`,
+    },
+  }
 }
 
 export default function BlogPostLayout({
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,62 +6,6 @@ import { Blog } from "@/types/blogs";
 
 type Params = Promise<{ slug: string }>
 
-export async function generateMetadata(
-  props: {
-    params: Params
-  },
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const blog = await getBlogData((await props.params).slug);
-  const defaultUrl = process.env.NEXT_PUBLIC_APP_URL
-    ? `https://www.${process.env.NEXT_PUBLIC_APP_URL}`
-    : "http://localhost:3000";
-  
-  if (!blog) {
-    return {
-      title: 'Blog Post Not Found',
-      description: 'The requested blog post could not be found.'
-    }
-  }
-
-  // Get the parent metadata
-  const previousImages = (await parent).openGraph?.images || []
-
-  return {
-    title: blog.title,
-    description: blog.excerpt || blog.content.substring(0, 160),
-    authors: [{ name: blog.author || 'Qalb Team' }],
-    openGraph: {
-      title: blog.title,
-      description: blog.excerpt || blog.content.substring(0, 160),
-      type: 'article',
-      publishedTime: blog.created_at,
-      authors: [blog.author || 'Qalb Team'],
-      url: `${defaultUrl}/blog/${(await props.params).slug}`,
-      siteName: 'Qalb',
-      locale: 'en_US',
-      images: blog.coverImage ? [
-        {
-          url: blog.coverImage,
-          width: 1200,
-          height: 630,
-          alt: blog.title,
-        },
-        ...previousImages
-      ] : previousImages,
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: blog.title,
-      description: blog.excerpt || blog.content.substring(0, 160),
-      creator: '@QalbApp',
-      images: blog.coverImage ? [blog.coverImage] : undefined,
-    },
-    alternates: {
-      canonical: `/blog/${(await props.params).slug}`,
-    },
-  }
-}
 
 
 export default async function BlogPost(props: {

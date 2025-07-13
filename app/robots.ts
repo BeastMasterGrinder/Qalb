aab768761d31e03e5978f49a6810d576159fc267
BeastMasterGrinder <m.farjad2004@gmail.com>
2025-07-13 19:11:53 +0500

    fix[sitemap][robots]: Remove the disable *.xml for all files

--- app/robots.ts
+++ app/robots.ts
@@ -25,7 +25,6 @@ export default function robots(): MetadataRoute.Robots {
         '/profile/*',
         '/private/*',
         '/*.json$',
-        '/*.xml$'
       ]
     },
     sitemap: `${baseUrl}/sitemap.xml`,
